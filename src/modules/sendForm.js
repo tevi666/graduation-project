caeea417549e21f9bfe64ad4a9e3443bcb762ea3
Təvəkkül <tevizeynalov@gmail.com>
2022-05-13 22:27:53 +0400

    fixed swiper

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -1,5 +1,8 @@
-export const sendForm = () => {
+export const sendForm = (
+    // formId,
+    // someElem = []
+) => {
     // const form = document.getElementById((formId));
-    const orderForm = document.querySelectorAll('.order-form');
-    console.log(orderForm);
+    // const orderForm = document.querySelectorAll('.order-form');
+
 };
