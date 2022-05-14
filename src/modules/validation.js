16f5637997648645f3afc7df34516bdcc892c71b
Təvəkkül <tevizeynalov@gmail.com>
2022-05-14 17:18:55 +0400

    feat validation

--- src/modules/validation.js
+++ src/modules/validation.js
@@ -1,43 +1,42 @@
 export const validateInput = (input) => {
-    if (input.value) {
-        if (input.name === 'fio') {
-            input.value = input.value.replace(/[^а-яА-Яa-zA-Z-\s]/g, '');
-        } else if (input.name === 'phone') {
-            input.value = input.value.replace(/[^\+\d]/g, '');
-        }
-    }
+	if (input.value) {
+		if (input.name === 'fio') {
+			input.classList.remove('error');
+			input.value = input.value.replace(/[^а-яА-Яa-zA-Z-\s]/g, '');
+		} else if (input.name === 'phone') {
+			input.classList.remove('error');
+			input.value = input.value.replace(/[^\+\d]/g, '');
+		}
+	}
 };
 
 export const isValidatedForm = (form) => {
-    const inputsList = form.querySelectorAll('input');
+	const inputsList = form.querySelectorAll('input');
 
-    let success = true;
-    let fioSuccess = false;
-    let phoneSuccess = false;
+	let fioSuccess = false;
+	let phoneSuccess = false;
 
-    if (inputsList) {
-        inputsList.forEach(input => {
-            if (input.value === '') {
-                success = false;
-            } else {
+	if (inputsList) {
+		inputsList.forEach(input => {
+			if (input.name === 'fio') {
+				input.value = input.value.trim();
+				fioSuccess = !!input.value && /[а-яА-Яa-zA-Z-\s]/g.test(input.value);
 
-                if (input.name === 'fio') {
-                    if (/[а-яА-Яa-zA-Z-\s]/g.test(input.value)) {
-                        fioSuccess = true;
-                    } else {
-                        fioSuccess = false;
-                    }
-                }
+				if (!fioSuccess) {
+					input.classList.add('error');
+				}
+			}
 
-                if ((input.name === 'phone')) {
-                    if (/^([+]?[0-9]{11,16})*$/g.test(input.value)) {
-                        phoneSuccess = true;
-                    } else {
-                        phoneSuccess = false;
-                    }
-                }
-            }
-        });
-    }
-    return success && fioSuccess && phoneSuccess;
+			if ((input.name === 'phone')) {
+				phoneSuccess = !!input.value && /^([+]?[0-9]{5,16})/g.test(input.value);
+
+				if (!phoneSuccess) {
+					input.classList.add('error');
+				}
+			}
+
+		});
+	}
+
+	return fioSuccess && phoneSuccess;
 };
