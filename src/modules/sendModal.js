0425b461b4081ec9f9cbb58245e37bbc5f69b11c
Təvəkkül <tevizeynalov@gmail.com>
2022-05-15 20:40:02 +0400

    feat sendModal

--- src/modules/sendModal.js
+++ src/modules/sendModal.js
@@ -1,3 +1,61 @@
+import {
+    validateInput,
+    isValidatedForm
+} from './validation';
+
 export const sendModal = () => {
-    console.log('sendModal');
+    const form = document.querySelectorAll('form[name="callback-form"]'),
+        form1 = document.querySelectorAll('form[name="application-form"]'),
+        inputs = document.querySelectorAll('input');
+
+
+    const postData = async (url, data) => {
+        let res = await fetch(url, {
+            method: "POST",
+            body: data
+        });
+        return await res.json();
+    };
+    inputs.forEach(item => {
+        item.addEventListener('input', e => {
+            if (e.target.matches('input')) {
+                validateInput(e.target);
+            }
+            return;
+        });
+    });
+    const clearInputs = () => {
+        inputs.forEach(item => {
+            item.value = '';
+        });
+    };
+
+    form.forEach(item => {
+        item.addEventListener('submit', e => {
+            e.preventDefault();
+            if (isValidatedForm(item)) {
+                const formData = new FormData(item);
+                postData('https://jsonplaceholder.typicode.com/posts', formData)
+                    .then(res => {
+                        console.log(res);
+                    })
+                    .catch(() => message);
+                clearInputs();
+            }
+        });
+    });
+    form1.forEach(item => {
+        item.addEventListener('submit', e => {
+            e.preventDefault();
+            if (isValidatedForm(item)) {
+                const formData = new FormData(item);
+                postData('https://jsonplaceholder.typicode.com/posts', formData)
+                    .then(res => {
+                        console.log(res);
+                    })
+                    .catch(() => message);
+                clearInputs();
+            }
+        });
+    });
 };
