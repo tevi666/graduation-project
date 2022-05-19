717daa5c9e75a94118e7a777942015cc4cc31db6
Təvəkkül <tevizeynalov@gmail.com>
2022-05-19 19:12:42 +0400

    sendModal error

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import {
 import {
     swiperBenefits,
     swiperServices
-} from './modules/swiper';
+} from "./modules/swiper";
 
 import {
     scroll
@@ -24,54 +24,103 @@ import {
 } from "./modules/sendForm";
 
 import {
-    comments
-} from './modules/comments';
+    menu
+} from "./modules/menu";
 
 import {
-    menu
-} from './modules/menu';
+    comments
+} from "./modules/comments";
 
-timer('20 may 2022 22:00');
+timer("20 may 2022 22:00");
 scroll();
 modal({
-	elementSelector: '.callback',
-	modalSelector: '.header-modal',
-	overlaySelector: '.overlay',
-	closeSelector: '.header-modal__close',
+    elementSelector: ".callback",
+    modalSelector: ".header-modal",
+    overlaySelector: ".overlay",
+    closeSelector: ".header-modal__close",
 });
 
 modal({
-	elementSelector: 'measurer-call',
-	modalSelector: '.services-modal',
-	overlaySelector: '.overlay',
-	closeSelector: '.services-modal__close',
-	isServices: true,
-	servicesBlockSelector: '#services',
+    elementSelector: "measurer-call",
+    modalSelector: ".services-modal",
+    overlaySelector: ".overlay",
+    closeSelector: ".services-modal__close",
+    isServices: true,
+    servicesBlockSelector: "#services",
 });
 
 modal({
-	elementSelector: '.document-overlay',
-	modalSelector: '.image-modal',
-	overlaySelector: '.overlay',
-	closeSelector: '.image-modal__close',
-	isImageModal: true,
-	boxElementSelector: '.sertificate-document',
-	modalBodySelector: '.image-modal__body'
+    elementSelector: ".document-overlay",
+    modalSelector: ".image-modal",
+    overlaySelector: ".overlay",
+    closeSelector: ".image-modal__close",
+    isImageModal: true,
+    boxElementSelector: ".sertificate-document",
+    modalBodySelector: ".image-modal__body",
 });
 calculate();
-sendForm({
-    formSelector: '[name="action-form"]',
-    additionalData: [{
-        type: 'input',
-        selector: '#calc-total'
-    }]
-});
-sendForm({
-    formSelector: '[name="action-form2"]',
-    additionalData: [{
-        type: 'input',
-        selector: '#calc-total'
-    }]
-});
-comments();
+const body = document.querySelector("body");
+if (body.classList.contains("okna")) {
+    sendForm({
+        formId: "form1",
+        someElement: [],
+    });
+    sendForm({
+        formId: "form2",
+        someElement: [],
+    });
+    sendForm({
+        formId: "callback-form1",
+        someElement: [],
+    });
+    sendForm({
+        formId: "application-form1",
+        someElement: [],
+    });
+}
+if (body.classList.contains("balkony")) {
+    sendForm({
+        formId: "form3",
+        someElement: [{
+            id: "calc-total",
+        }, ],
+    });
+    sendForm({
+        formId: "form4",
+        someElement: [{
+            id: "calc-total",
+        }, ],
+    });
+    sendForm({
+        formId: "callback-form2",
+        someElement: [{
+            id: "calc-total",
+        }, ],
+    });
+    sendForm({
+        formId: "application-form2",
+        someElement: [{
+            id: "calc-total",
+        }, ],
+    });
+}
+if (body.classList.contains("kuhni")) {
+    sendForm({
+        formId: "form5",
+        someElement: [],
+    });
+    sendForm({
+        formId: "form6",
+        someElement: [],
+    });
+    sendForm({
+        formId: "callback-form3",
+        someElement: [],
+    });
+    sendForm({
+        formId: "application-form3",
+        someElement: [],
+    });
+}
 menu();
+comments();
--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -1,135 +1,75 @@
 import {
 	validateInput,
 	isValidatedForm
-} from './validation';
-
+} from "./validation";
 export const sendForm = ({
-	formSelector,
-	additionalData = []
+	formId,
+	someElement = []
 }) => {
-	const form = document.querySelector(formSelector);
-	const modal = document.querySelector('.header-modal--opened');
-	const modalOverlay = document.querySelector('.overlay');
-	const modal1 = document.querySelector('.services-modal--opened');
-	const statusBlock = document.createElement('div');
-
-	statusBlock.style.textAlign = 'center';
-	statusBlock.style.margin = '10px';
-	statusBlock.style.color = '#546a76';
-	statusBlock.style.opacity = 0.7;
-
-	try {
-		form.append(statusBlock);
-	} catch (error) {
-		console.log(error.message);
-	}
-
-	const noneModal = () => {
-		modal.style.display = 'none';
-	};
-	const noneModalOverlay = () => {
-		modalOverlay.style.display = 'none';
-	};
-	const noneModal1 = () => {
-		modal1.style.display = 'none';
-	};
-
+	const body = document.querySelector("body");
+	const form = document.getElementById(formId);
 	const sendData = (data) => {
-		return fetch('https://jsonplaceholder.typicode.com/posts', {
-			method: 'POST',
-			body: JSON.stringify(data),
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		}).then(res => res.json());
+		return fetch("https://jsonplaceholder.typicode.com/posts", {
+				method: "POST",
+				body: JSON.stringify(data),
+				headers: {
+					"Content-type": "application/json; charset=UTF-8",
+				},
+			})
+			.then((response) => {
+				if (response.status !== 404) {
+					return response.json();
+				} else {
+					console.log("Ошибка отправки");
+				}
+			})
+			.catch((error) => {
+				console.log(error);
+			});
 	};
-
-	const showMessage = (status) => {
-		statusBlock.innerHTML = '';
-		switch (status) {
-			case 'sending':
-				statusBlock.innerHTML = `Отправка..`;
-				break;
-			case 'error':
-				statusBlock.innerHTML = `Ошибка отправки данных. Попробуйте позднее`;
-				setTimeout(() => {
-					statusBlock.innerHTML = '';
-				}, 5000);
-				break;
-			case 'success':
-				statusBlock.innerHTML = `Данные успешно отправлены`;
-				setTimeout(() => {
-					statusBlock.innerHTML = '';
-				}, 3000);
-				break;
-			case 'not valid':
-				statusBlock.innerHTML = `Проверьте правильность введенных данных`;
-				break;
-		}
-	};
-
 	const submitForm = () => {
-		showMessage('sending');
-		const formElements = form.querySelectorAll('input');
+		const formElements = form.querySelectorAll("input[type=text]");
 		const formData = new FormData(form);
-		const fromBody = {};
+		const formBody = {};
+		let calcTotal = document.getElementById("calc-total");
 
 		formData.forEach((val, key) => {
-			fromBody[key] = val;
+			formBody[key] = val;
 		});
 
-		if (additionalData.length > 0) {
-			additionalData.forEach(elem => {
-				const element = document.querySelector(elem.selector);
-				if (element) {
-					fromBody[element.id] = elem.type === 'block' ? element.textContent : element.value;
-				}
-			});
-		}
-
-		sendData(fromBody)
-			.then(data => {
-				showMessage('success');
-				formElements.forEach(input => {
-					input.value = '';
+		if (body.classList.contains("balkony")) {
+			if (calcTotal.value !== "") {
+				someElement.forEach((elem) => {
+					const element = document.getElementById(elem.id);
+					formBody[elem.id] = element.value;
 				});
-			}).catch(error => {
-				showMessage('error');
-				console.log(error.message);
-				setTimeout(() => {
-					formElements.forEach(input => {
-						input.value = '';
-					});
-				}, 5000);
+			}
+		}
+		form.addEventListener("submit", (e) => {
+			e.preventDefault();
+			submitForm();
+		});
+		sendData(formBody)
+			.then((data) => {
+				formElements.forEach((input) => {
+					input.value = "";
+				});
+			})
+			.catch((error) => {
+				console.log(error);
 			});
 	};
 
 	try {
 		if (!form) {
-			throw new Error(`form ${formSelector} is not found`);
+			throw new Error("Верните форму на страницу");
 		}
 
-		form.addEventListener('input', e => {
-			if (e.target.matches('input')) {
-				validateInput(e.target);
-			}
-			return;
-		});
-
-		form.addEventListener('submit', e => {
+		form.addEventListener("submit", (e) => {
 			e.preventDefault();
-			if (isValidatedForm(form)) {
-				submitForm();
-				setTimeout(() => {
-					noneModal();
-					noneModal1();
-					noneModalOverlay();
-				}, 4000);
-			} else {
-				showMessage('not valid');
-			}
+			submitForm();
 		});
 	} catch (error) {
-		console.log(error);
+		console.log(error.message);
 	}
 };
